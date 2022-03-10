deb32afc0e3061a6391a68bc6bb5430be4e6c4e1
Adrian Harwood <adrian.harwood@manchester.ac.uk>
2022-03-10 10:13:29 +0000

    Added template app

---
+++ PPMTool/Pages/Capacity - Copy.razor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using PPMTool.Data;
+
+namespace PPMTool.Pages
+{
+    public partial class Capacity : ComponentBase
+    {
+        private WeatherForecast[] data;
+
+        protected override async Task OnInitializedAsync()
+        {
+            data = await ForecastService.GetForecastAsync(DateTime.Now);
+        }
+    }
+}
---
+++ PPMTool/Pages/Users.razor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using PPMTool.Data;
+
+namespace PPMTool.Pages
+{
+    public partial class Users : ComponentBase
+    {
+        private WeatherForecast[] data;
+
+        protected override async Task OnInitializedAsync()
+        {
+        }
+    }
+}
