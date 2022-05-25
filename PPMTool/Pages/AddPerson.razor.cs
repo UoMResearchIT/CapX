b116263c85cad8f0832f068baaebd04c742c0cdd
Adrian Harwood <adrian.harwood@manchester.ac.uk>
2022-05-25 11:53:22 +0100

    Added placeholder add pages

---
+++ PPMTool/Pages/AddPerson.razor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using PPMTool.Data;
+using PPMTool.Services;
+
+namespace PPMTool.Pages
+{
+    public partial class AddPerson : ComponentBase
+    {
+        [Inject]
+        private PersonService PersonService { get; }
+
+        private Person[] people;
+
+        protected override async Task OnInitializedAsync()
+        {
+        }
+    }
+}
---
+++ PPMTool/Pages/AddProject.razor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using PPMTool.Data;
+using PPMTool.Services;
+
+namespace PPMTool.Pages
+{
+    public partial class AddProject : ComponentBase
+    {
+        [Inject]
+        private ProjectService ProjectService { get; }
+
+        protected override async Task OnInitializedAsync()
+        {
+        }
+    }
+}
