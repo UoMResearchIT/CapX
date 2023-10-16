848eeaf70e0150774ed543e910bf2a335e4c859a
Adrian Harwood <adrian.harwood@manchester.ac.uk>
2023-10-16 16:01:42 +0100

    Added local and non-local login options with a build config

--- PPMTool/Pages/Account/Login.cshtml.cs
+++ PPMTool/Pages/Account/Login.cshtml.cs
@@ -1,8 +1,12 @@
+#if !LOCAL
+using System.Threading.Tasks;
+#else
 using System.Security.Claims;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+#endif
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace PPMTool.Pages.Account
@@ -10,13 +14,14 @@ namespace PPMTool.Pages.Account
     [AllowAnonymous]
     public class LoginModel : PageModel
     {
-        //public async Task OnGet(string redirectUri)
-        //{
-        //    // Challenge to force authentication
-        //    var props = new AuthenticationProperties { RedirectUri = "/" };
-        //    await HttpContext.ChallengeAsync("CAS", props);
-        //}
-
+#if !LOCAL
+        public async Task OnGet(string redirectUri)
+        {
+            // Challenge to force authentication
+            var props = new AuthenticationProperties { RedirectUri = "/" };
+            await HttpContext.ChallengeAsync("CAS", props);
+        }
+#else
         public IActionResult OnGet(string scheme)
         {
             // Local debugging so just sign in
@@ -26,5 +31,6 @@ namespace PPMTool.Pages.Account
                 new AuthenticationProperties { RedirectUri = "/" },
                 CookieAuthenticationDefaults.AuthenticationScheme);
         }
+#endif
     }
 }
