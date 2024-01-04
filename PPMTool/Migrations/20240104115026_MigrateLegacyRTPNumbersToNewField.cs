cc36fbc2037dbfb85e2e9bf7c53922c40b139977
AdrianHarwood <adrian.harwood@manchester.ac.uk>
2024-01-04 12:26:44 +0000

    Added migration to do the RTP number mapping properly for legacy entries

---
+++ PPMTool/Migrations/20240104115026_MigrateLegacyRTPNumbersToNewField.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PPMTool.Migrations
+{
+    public partial class MigrateLegacyRTPNumbersToNewField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"
+                    UPDATE Projects
+                    SET RTP = SUBSTR(Name, 5, INSTR(Name, ' ') - 5), Name = SUBSTR(Name, INSTR(Name, ' ') + 1)
+                    WHERE Name LIKE 'RTP-%'
+                "
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"
+                    UPDATE Projects
+                    SET Name = 'RTP-' || RTP || ' ' || Name,
+                    RTP = 0;
+                "
+            );
+        }
+    }
+}
