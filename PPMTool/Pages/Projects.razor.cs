bd9385b71671ff0d840fcd4cbea45f79588bd403
AdrianHarwood <adrian.harwood@manchester.ac.uk>
2024-01-05 20:17:57 +0000

    Improved performance of the project grid by removing old feature.

--- PPMTool/Pages/Projects.razor.cs
+++ PPMTool/Pages/Projects.razor.cs
@@ -2,14 +2,11 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Components;
 using PPMTool.Data.Context;
 using PPMTool.Data.Entities;
 using PPMTool.Enums;
-using PPMTool.Pages.Components;
 using PPMTool.Services;
-using Radzen.Blazor;
 
 namespace PPMTool.Pages
 {
@@ -20,7 +17,6 @@ namespace PPMTool.Pages
 
         private IEnumerable<Project> projects;
         private IEnumerable<Portfolio> portfolioOptions = (Portfolio[])Enum.GetValues(typeof(Portfolio));
-        private List<ProjectSummaryWidget.ProjectSummaryData> summaryData;
         private PPMToolContext context;
         private bool showActiveOnly = true;
 
@@ -41,21 +37,6 @@ namespace PPMTool.Pages
             // Get projects from the database
             context = new PPMToolContext();
             var proj = ProjectService.GetAll(context).OrderBy(x => x.RTP).ToList();
-
-            // Build the summary widget data and sort by total
-            summaryData = new List<ProjectSummaryWidget.ProjectSummaryData>();
-            foreach (var p in portfolioOptions)
-            {
-                summaryData.Add(new ProjectSummaryWidget.ProjectSummaryData
-                {
-                    Portfolio = p,
-                    Active = proj.Where(x => x.Portfolio == p && (x.ProjectStatus == ProjectStatus.Active || x.ProjectStatus == ProjectStatus.Paused || x.ProjectStatus == ProjectStatus.Maintenance)).Count(),
-                    Incoming = proj.Where(x => x.Portfolio == p && (x.ProjectStatus == ProjectStatus.Unfunded || x.ProjectStatus == ProjectStatus.Funded)).Count(),
-                    Complete = proj.Where(x => x.Portfolio == p && x.ProjectStatus == ProjectStatus.Finished).Count()
-                });
-            }
-            summaryData = summaryData.OrderByDescending(x => x.GetTotal()).ToList();
-
 
             // Remove the ones that are not active for the data grid if necessary
             if (showActiveOnly) proj = proj.Where(x => !x.ProjectStatus.IsProjectFinishedOrCancelled()).ToList();
