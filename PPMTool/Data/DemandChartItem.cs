3c2d3811accbc3f662ee859b0dd61d8b97616954
AdrianHarwood <adrian.harwood@manchester.ac.uk>
2024-08-01 19:48:24 +0100

    First pass at the algorithm to generate the data required

--- PPMTool/Data/DemandChartItem.cs
+++ PPMTool/Data/DemandChartItem.cs
@@ -7,119 +7,161 @@ namespace PPMTool.Data
         /// <summary>
         /// Start of the week to which this data corresponds
         /// </summary>
-        public DateTime WeekStart { get; }
+        private DateTime weekStart;
+        public DateTime WeekStart
+        {
+            get => weekStart;
+            set
+            {
+                if (value != weekStart)
+                {
+                    weekStart = value;
+                    Period = (int)Math.Ceiling(weekStart.Month / 3f);
+                }
+            }
+        }
 
         /// <summary>
-        /// Set automatically when the week is set and represents the period of the year as a string
+        /// Set automatically when the week is set and represents the period of the year
         /// </summary>
-        public string PeriodAsString { get; }
+        public int? Period { get; private set; }
 
 
         // Workload Model //
 
         /// <summary>
-        /// Number of staff active in this week
-        /// </summary>
-        public int NumberOfStaff { get; }
-
-        /// <summary>
         /// FTE available to do project work
         /// </summary>
-        public float ProjectFTE { get; }
+        public float ProjectFTE { get; set; }
 
         /// <summary>
         /// FTE available to do BAU
         /// </summary>
-        public float BAUFTE { get; }
+        public float BAUFTE { get; set; }
 
         /// <summary>
         /// FTE available to do Personal Development
         /// </summary>
-        public float PersonalDevFTE { get; }
+        public float PersonalDevFTE { get; set; }
 
         /// <summary>
         /// FTE available to do PSM
         /// </summary>
-        public float PSMFTE { get; }
+        public float PSMFTE { get; set; }
 
         /// <summary>
         /// FTE available to do Line Management
         /// </summary>
-        public float StaffManFTE { get; }
+        public float StaffManFTE { get; set; }
 
         /// <summary>
         /// FTE available to do RSA work
         /// </summary>
-        public float RSAFTE { get; }
-
-        /// <summary>
-        /// Total amount of FTE assgined to projects
-        /// </summary>
-        public float AssignedFTE { get; }
+        public float RSAFTE { get; set; }
 
         /// <summary>
         /// Total FTE that is available for project work that is not assigned this week
         /// </summary>
-        public float UnassignedFTE { get; }
+        public float UnassignedFTE { get; set; }
+
+        /// <summary>
+        /// The amount of FTE that represents allocation of staff to project work beyond their workload model limits
+        /// </summary>
+        public float OverallocationFTE { get; set; }
+
+        /// <summary>
+        /// The amount of FTE that represents underallocation of staff to project work based on their workload model
+        /// </summary>
+        public float UnderallocationFTE { get; set; }
 
 
         // Demand Data //
 
         /// <summary>
+        /// Number of staff active in this week
+        /// </summary>
+        public int NumberOfStaff { get; set; }
+
+        /// <summary>
         /// Essentially the number of active staff minus the number of staff the Head of RSE is managing
         /// </summary>
-        public int NumberStaffRequiringLineManagement { get; }
+        public int NumberStaffRequiringLineManagement { get; set; }
+
+        /// <summary>
+        /// The number of projects that are confirmed
+        /// </summary>
+        public int NumberOfConfirmedProjects { get; set; }
+
+        /// <summary>
+        /// The number of projects that are unconfirmed
+        /// </summary>
+        public int NumberOfUnconfirmedProjects { get; set; }
 
         /// <summary>
         /// Demand for projects with the current week that is not being met by an assignment
         /// </summary>
-        public float UnmetDemandFTE { get; }
+        public float UnmetDemandFTE { get; set; }
 
         /// <summary>
         /// Total FTE in assignments within the current week
         /// </summary>
-        public float MetDemandFTE { get; }
+        public float MetDemandFTE { get; set; }
 
         /// <summary>
         /// The difference between the unassiged FTE and the unmet demand FTE
-        /// (i.e. even if we were to somehow assign all free project work FTE, how much would we still be short by)
+        /// (i.e. even if we were to somehow assign all free project work FTE, how much spare project FTE would we have)
         /// </summary>
-        public float TechnicalEffortShortfall { get; }
+        public float BenchProjectFTE { get; set; }
 
         /// <summary>
         /// The sum of the met and unmet demand this week
         /// </summary>
-        public float TotalDemand { get; }
+        public float TotalDemandFTE { get; set; }
 
         /// <summary>
         /// Amount of FTE of confirmed projects this week
         /// </summary>
-        public float ConfirmedProjectFTE { get; }
+        public float ConfirmedDemandFTE { get; set; }
 
         /// <summary>
         /// Amount of FTE of projects that are not yet confirmed this week
         /// </summary>
-        public float UnconfirmedProjectFTE { get; }
+        public float UnconfirmedDemandFTE { get; set; }
 
         /// <summary>
         /// Amount of unmet demand on confirmed projects this week
         /// </summary>
-        public float ConfirmedUnallocatedProjectFTE { get; }
+        public float ConfirmedUnmetDemandFTE { get; set; }
 
         /// <summary>
         /// Amount of met demand on unconfirmed projects this week
         /// </summary>
-        public float UnconfirmedAllocatedProjectFTE { get; }
+        public float UnconfirmedMetDemandFTE { get; set; }
 
         /// <summary>
         /// Amount of met demand on confirmed projects this week
         /// </summary>
-        public float ConfirmedAllocatedProjectFTE { get; }
+        public float ConfirmedMetDemandFTE { get; set; }
 
         /// <summary>
         /// Amount of unmet demand on unconfirmed projects this week
         /// </summary>
-        public float UnconfirmedUnllocatedProjectFTE { get; }
+        public float UnconfirmedUnmetDemandFTE { get; set; }
+
+        /// <summary>
+        /// Amount of unmet demand on cancelled projects this week
+        /// </summary>
+        public float CancelledDemand { get; set; }
+
+        /// <summary>
+        /// Amount of met demand on finished projects this week
+        /// </summary>
+        public float FinishedMetDemand { get; set; }
+
+        /// <summary>
+        /// Amount of unmet demand on finished projects this week
+        /// </summary>
+        public float FinishedUnmetDemand { get; set; }
 
 
         // Cost Data //
@@ -127,12 +169,12 @@ namespace PPMTool.Data
         /// <summary>
         /// Based on the standard cost model what is the value of the confrimed projects
         /// </summary>
-        public float ConfirmedValue { get; }
+        public float ConfirmedValue { get; set; }
 
         /// <summary>
         /// Based on the standard cost model what is the value of the unconfrimed projects
         /// </summary>
-        public float UnconfirmedValue { get; }
+        public float UnconfirmedValue { get; set; }
 
     }
 }
--- PPMTool/Pages/DataDashboard.razor.cs
+++ PPMTool/Pages/DataDashboard.razor.cs
@@ -1,21 +1,224 @@
-﻿namespace PPMTool.Pages
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using PPMTool.Data;
+using PPMTool.Data.Entities;
+using PPMTool.Enums;
+using PPMTool.Services;
+
+namespace PPMTool.Pages
 {
     public partial class DataDashboard : BasePage
     {
-        private float personalDevFTE;
-        private float architectureFTE;
-        private float projectManFTE;
-        private float staffManFTE;
-        private float coachFTE;
+        private float personalDevFTE = 0.1f;
+        private float architectureFTE = 0.05f;
+        private float projectManFTE = 0.05f;
+        private float staffManFTE = 0.05f;
+        private float coachFTE = 0.1f;
 
-        private float grade41Costs;
-        private float grade55Costs;
-        private float grade65Costs;
-        private float grade71Costs;
-        private float grade75Costs;
+        private float grade41Costs = 33333.55f;
+        private float grade55Costs = 43172.16f;
+        private float grade65Costs = 50935.80f;
+        private float grade71Costs = 57458.16f;
+        private float grade75Costs = 64797.29f;
 
-        private float currentBudget;
+        private float currentBudget = 1096765;
+        private int numberOfStaffManagedByHead = 6;
+        private DateTime startDate = DateTime.Today;
+        private int yearsBehind = 1;
+        private int yearsAhead = 2;
 
-        private int numberOfStaffManagedByHead;
+        private List<DemandChartItem> demandChartItems = new List<DemandChartItem>();
+
+        [Inject]
+        private PersonService PersonService { get; set; }
+
+        [Inject]
+        private ProjectService ProjectService { get; set; }
+
+        public DataDashboard()
+        {
+            // Default to the first day of the current financial year
+            var today = DateTime.Today;
+            startDate = new DateTime(today.Month < 8 ? today.Year - 1 : today.Year, 8, 1);
+        }
+
+        private void GenerateCharts()
+        {
+            Loading = true;
+            Task.Run(() =>
+            {
+                // Clear the existing demand item list
+                demandChartItems.Clear();
+
+                // Get starting lists from the DB
+                var people = PersonService.GetAll(context);
+                var projects = ProjectService.GetAll(context);
+
+                // Initialise
+                float wlmProject = 0f;
+                float wlmBAU = 0f;
+                float wlmPD = 0f;
+                float wlmPSM = 0f;
+                float wlmStaff = 0f;
+                float wlmRSA = 0f;
+                float assignmentUnder = 0f;
+                float assignmentOver = 0f;
+                int numStaff = 0;
+
+                // For each week
+                var currentWeekStart = startDate.AddYears(-yearsBehind);
+                while (currentWeekStart < startDate.AddYears(yearsAhead))
+                {
+                    // Get the projects that are running during the week (exclude those projects with no tasks that have default start date)
+                    var projectsInDatabaseThisWeek = projects.Where(x => x.StartDate != default && x.IsWithin(currentWeekStart, currentWeekStart.AddDays(6)));
+
+
+                    // Cancelled //
+                    var tasksOnCancelledProjectsThisWeek = projectsInDatabaseThisWeek.Where(x => x.ProjectStatus.IsCancelled()).SelectMany(x => x.SubTasks.Where(x => x.IsWithin(currentWeekStart, currentWeekStart.AddDays(6))));
+                    var cancelledDemand = (float)tasksOnCancelledProjectsThisWeek.RoundedSum(x => x.Demand);
+
+
+                    // All Projects (not cancelled) //
+
+                    // Get projects not cancelled
+                    var projectsThisWeekNotCancelled = projectsInDatabaseThisWeek.Where(x => !x.ProjectStatus.IsCancelled());
+
+                    // Get number of confirmed and unconfirmed in this subset
+                    var numberUnconfirmed = projectsThisWeekNotCancelled.Where(x => x.ProjectStatus.IsUnconfirmed()).Count();
+                    var numberConfirmed = projectsThisWeekNotCancelled.Count() - numberUnconfirmed;
+
+                    // Get all tasks that run during the week
+                    var tasksOnActiveProjectsThisWeek = projectsThisWeekNotCancelled.SelectMany(x => x.SubTasks.Where(x => x.IsWithin(currentWeekStart, currentWeekStart.AddDays(6))));
+
+                    // Get demand totals from tasks
+                    var unmetDemand = (float)tasksOnActiveProjectsThisWeek.RoundedSum(x => x.UnmetDemand);
+                    var totalDemand = (float)tasksOnActiveProjectsThisWeek.RoundedSum(x => x.Demand);
+                    var metDemand = totalDemand - unmetDemand;
+
+
+                    // Finished //
+
+                    // Get just total FTE of finished projects
+                    var tasksOnFinishedProjectsThisWeek = projectsThisWeekNotCancelled.Where(x => x.ProjectStatus == ProjectStatus.Finished).SelectMany(x => x.SubTasks.Where(x => x.IsWithin(currentWeekStart, currentWeekStart.AddDays(6))));
+                    var metDemandFinished = (float)tasksOnFinishedProjectsThisWeek.RoundedSum(x => x.Demand);
+                    var unmetDemandFinished = (float)tasksOnFinishedProjectsThisWeek.RoundedSum(x => x.UnmetDemand);
+
+
+                    // Confirmed //
+
+                    // Get just confirmed project tasks
+                    var tasksOnConfirmedActiveProjectsThisWeek = projectsThisWeekNotCancelled.Where(x => !x.ProjectStatus.IsUnconfirmed()).SelectMany(x => x.SubTasks.Where(x => x.IsWithin(currentWeekStart, currentWeekStart.AddDays(6))));
+
+                    // Get met and unmet demand for this subset
+                    var unmetDemandConfirmed = (float)tasksOnConfirmedActiveProjectsThisWeek.RoundedSum(x => x.UnmetDemand);
+                    var totalDemandConfirmed = (float)tasksOnConfirmedActiveProjectsThisWeek.RoundedSum(x => x.Demand);
+                    var metDemandConfirmed = totalDemandConfirmed - unmetDemandConfirmed;
+
+
+                    // Unconfirmed //
+
+                    // Calculate the unconfirmed totals
+                    var unmetDemandUnconfirmed = unmetDemand - unmetDemandConfirmed;
+                    var totalDemandUnconfirmed = totalDemand = totalDemandConfirmed;
+                    var metDemandUnconfirmed = totalDemandUnconfirmed - unmetDemandUnconfirmed;
+
+                    // Compute value of confirmed and unconfirmed projects using G7.1 salary costs
+                    var confirmedValue = (float)Math.Round(totalDemandConfirmed * grade71Costs, 2);
+                    var unConfirmedValue = (float)Math.Round(totalDemandUnconfirmed * grade71Costs, 2);
+
+
+                    // People //
+
+                    // Get the people who are employed for at least one day during the week
+                    var peopleEmployedThisWeek = people.Where(x => x.StartDate <= currentWeekStart && (x.EndDate == null || x.EndDate >= currentWeekStart));
+
+                    // Compute people based totals
+                    foreach (var person in peopleEmployedThisWeek)
+                    {
+                        // Get the workload model that is active at the beginning of the week
+                        var activeModel = person.WorkloadModelChanges.Where(x => x.ChangeDate <= currentWeekStart).OrderBy(x => x.ChangeDate).LastOrDefault();
+
+                        // If no workload model active then default to the standard 100% project work model
+                        if (activeModel == null)
+                        {
+                            activeModel = new WorkloadModelChange()
+                            {
+                                ChangeDate = currentWeekStart,
+                                Person = person,
+                                ProjectWorkFTE = person.FTE,
+                                Notes = "Default model"
+                            };
+                        }
+
+                        // Update totals
+                        wlmProject += (float)activeModel.ProjectWorkFTE;
+                        wlmBAU += (float)activeModel.BusinessAsUsualFTE;
+                        wlmPD += (float)activeModel.PersonalDevelopmentFTE;
+                        wlmPSM += (float)activeModel.ProjectAndServiceManagementFTE;
+                        wlmStaff += (float)activeModel.StaffManagementFTE;
+                        wlmRSA += (float)activeModel.ArchitectureFTE;
+                        numStaff++;
+
+                        // Get assignments for this person and sum for the week
+                        var assignmentsThisWeek = tasksOnActiveProjectsThisWeek.SelectMany(x => x.AssignedResources.Where(x => x.Person.PersonId == person.PersonId));
+                        var totalAssignmentFTE = assignmentsThisWeek.RoundedSum(x => x.AssignmentFTE);
+
+                        // Increment the totals of under and overallocation
+                        if (totalAssignmentFTE > activeModel.ProjectWorkFTE)
+                        {
+                            assignmentOver += (float)(totalAssignmentFTE - activeModel.ProjectWorkFTE);
+                        }
+                        else if (totalAssignmentFTE < activeModel.ProjectWorkFTE)
+                        {
+                            assignmentUnder += (float)(activeModel.ProjectWorkFTE - totalAssignmentFTE);
+                        }
+                    }
+
+                    // Create a demand item and add it to the list
+                    demandChartItems.Add(new DemandChartItem()
+                    {
+                        WeekStart = currentWeekStart,
+                        ProjectFTE = wlmProject,
+                        BAUFTE = wlmBAU,
+                        PersonalDevFTE = wlmPD,
+                        PSMFTE = wlmPSM,
+                        StaffManFTE = wlmStaff,
+                        RSAFTE = wlmRSA,
+                        NumberOfStaff = numStaff,
+                        NumberStaffRequiringLineManagement = numStaff - numberOfStaffManagedByHead,
+                        NumberOfConfirmedProjects = numberConfirmed,
+                        NumberOfUnconfirmedProjects = numberUnconfirmed,
+                        UnmetDemandFTE = unmetDemand,
+                        MetDemandFTE = metDemand,
+                        TotalDemandFTE = totalDemand,
+                        UnassignedFTE = wlmProject - metDemand,
+                        ConfirmedMetDemandFTE = metDemandConfirmed,
+                        ConfirmedUnmetDemandFTE = unmetDemandConfirmed,
+                        UnconfirmedMetDemandFTE = metDemandUnconfirmed,
+                        UnconfirmedUnmetDemandFTE = unmetDemandUnconfirmed,
+                        ConfirmedDemandFTE = totalDemandConfirmed,
+                        UnconfirmedDemandFTE = totalDemandUnconfirmed,
+                        UnderallocationFTE = assignmentUnder,
+                        OverallocationFTE = assignmentOver,
+                        BenchProjectFTE = wlmProject - metDemand - unmetDemand,
+                        ConfirmedValue = confirmedValue,
+                        UnconfirmedValue = unConfirmedValue,
+                        CancelledDemand = cancelledDemand,
+                        FinishedMetDemand = metDemandFinished,
+                        FinishedUnmetDemand = unmetDemandFinished,
+                    });
+
+                    // Move to next week
+                    currentWeekStart = currentWeekStart.AddDays(7);
+                }
+
+            }).ContinueWith(t =>
+            {
+                Loading = false;
+            });
+        }
     }
 }
