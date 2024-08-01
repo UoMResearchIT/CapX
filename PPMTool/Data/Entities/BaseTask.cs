7050f21b926922368b938724d33216d128103341
AdrianHarwood <adrian.harwood@manchester.ac.uk>
2024-08-01 19:48:04 +0100

    Moved the within methods to the base task

--- PPMTool/Data/Entities/BaseTask.cs
+++ PPMTool/Data/Entities/BaseTask.cs
@@ -22,5 +22,30 @@ namespace PPMTool.Data.Entities
         public double PlannedCost { get; set; }
 
         public double ActualCost { get; set; }
+
+        /// <summary>
+        /// Method to determine whether a date is in the range [task.startDate task.endDate].
+        /// If end date and start date are the same evaluates against start date.
+        /// </summary>
+        /// <param name="testDate">Date to test</param>
+        /// <returns></returns>
+        internal bool IsWithin(DateTime testDate)
+        {
+            return StartDate.Date == EndDate.Date ? testDate.Date == StartDate.Date : testDate.Date >= StartDate.Date && testDate.Date <= EndDate.Date;
+        }
+
+        /// <summary>
+        /// Method to determine whether any part of the task runs within a date range [startDate endDate].
+        /// </summary>
+        /// <param name="startDate"></param>
+        /// <param name="endDate"></param>
+        /// <returns></returns>
+        internal bool IsWithin(DateTime startDate, DateTime endDate)
+        {
+            return
+                IsWithin(endDate) ||
+                IsWithin(startDate) ||
+                StartDate.Date <= startDate.Date && EndDate.Date >= endDate.Date;
+        }
     }
 }
