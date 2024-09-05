f5bec8f90fcd900f55755cf1ce00fbc2a3f4eef7
AdrianHarwood <adrian.harwood@manchester.ac.uk>
2024-09-05 16:35:26 +0100

    Added financial reference entity and migration

---
+++ PPMTool/Data/Entities/FinancialReference.cs
@@ -0,0 +1,21 @@
+﻿namespace PPMTool.Data.Entities
+{
+    public class FinancialReference
+    {
+        public int FinancialReferenceId { get; set; }
+
+        public int FinancialYear { get; set; }
+
+        public float Grade41Costs { get; set; }
+
+        public float Grade55Costs { get; set; }
+
+        public float Grade65Costs { get; set; }
+
+        public float Grade71Costs { get; set; }
+
+        public float Grade75Costs { get; set; }
+
+        public float RecoveryTarget { get; set; }
+    }
+}
