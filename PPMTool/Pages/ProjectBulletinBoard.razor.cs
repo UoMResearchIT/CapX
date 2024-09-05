b91acc4bfb418d40cf8ce2dd7913c63aa768ca0b
AdrianHarwood <adrian.harwood@manchester.ac.uk>
2024-09-05 10:10:44 +0100

    Added controls

--- PPMTool/Pages/ProjectBulletinBoard.razor.cs
+++ PPMTool/Pages/ProjectBulletinBoard.razor.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
@@ -18,6 +19,11 @@ namespace PPMTool.Pages
         private ProjectService ProjectService { get; set; }
 
         private IEnumerable<Project> availableProjects;
+        private IEnumerable<Project> allProjects;
+        private DateTime? startDate;
+        private DateTime? endDate;
+        private bool groupByStatus;
+
 
         protected override void OnInitialized()
         {
@@ -36,13 +42,45 @@ namespace PPMTool.Pages
             }
         }
 
+        private void ClearDateFilter()
+        {
+            startDate = null;
+            endDate = null;
+            FilterProjects();
+        }
+
+        private void FilterProjects()
+        {
+            if (allProjects == null) return;
+
+            availableProjects = allProjects;
+
+            if (startDate != null)
+            {
+
+            }
+
+            if (endDate != null)
+            {
+
+            }
+
+            if (groupByStatus)
+            {
+
+            }
+        }
+
         private void LoadProjectData()
         {
             // Get projects from the database
             var proj = ProjectService.GetAll(context).OrderBy(x => x.RTP).ToList();
 
-            // Filter owned projects to only show active ones
-            availableProjects = proj.Where(x => !x.ProjectStatus.IsFinishedOrCancelled() && x.HasUnmetDemandNowOrInFuture());
+            // Filter to just projects that are active with current or future unmet demand
+            allProjects = proj.Where(x => !x.ProjectStatus.IsFinishedOrCancelled() && x.HasUnmetDemandNowOrInFuture());
+
+            // Filter the projects
+            FilterProjects();
 
             // Disable spinner now load complete
             Loading = false;
