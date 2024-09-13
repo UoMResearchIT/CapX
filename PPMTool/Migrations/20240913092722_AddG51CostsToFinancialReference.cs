85add8cf98f4cbc3affedf36e4e74cb02dbfb341
AdrianHarwood <adrian.harwood@manchester.ac.uk>
2024-09-13 10:27:47 +0100

    Added extra financial reference

---
+++ PPMTool/Migrations/20240913092722_AddG51CostsToFinancialReference.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PPMTool.Migrations
+{
+    public partial class AddG51CostsToFinancialReference : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "Grade51Costs",
+                table: "FinancialReferences",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Grade51Costs",
+                table: "FinancialReferences");
+        }
+    }
+}
