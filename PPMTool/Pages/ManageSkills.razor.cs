15c5a71d85c433004f6fb22098a09e2b78da3bc4
Chris Lam <fdq09eca@gmail.com>
2024-09-19 23:40:56 +0100

    Skill Page Fix bug

--- PPMTool/Pages/ManageSkills.razor.cs
+++ PPMTool/Pages/ManageSkills.razor.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components;
+using PPMTool.Data;
 using PPMTool.Data.Entities;
 using PPMTool.Services;
 using Radzen;
@@ -17,12 +18,40 @@ namespace PPMTool.Pages
         [Inject]
         private TagService TagService { get; set; }
 
+        private StatusMessage statusMessage;
+        
+        private bool IsDuplicatedSkill(SkillTag entity)
+        {
+            if (TagService.DuplicateDetected(context, entity))
+            {
+                statusMessage = new StatusMessage("An entry with the same skill already exists.", StatusMessage.MessageType.Error);
+                return true;
+            }
+            statusMessage = null;
+            return false;
+        }
+
+
         protected override void OnInitialized()
         {
             base.OnInitialized();
             dataGridEntityService = TagService;
             dataGridEntities = TagService.GetAll(context).OrderBy(x => x.Name).ToList();
             LogInformation($"Viewing skills tags");
+        }
+
+
+        protected override async Task EditRow(SkillTag entity)
+        {
+            if (IsDuplicatedSkill(entity)) return;
+            await base.EditRow(entity);
+
+        }
+
+        protected override async Task SaveRow(SkillTag entity)
+        {
+            if (IsDuplicatedSkill(entity)) return;
+            await base.SaveRow(entity);
         }
 
         protected override async Task DeleteRow(SkillTag entity)
