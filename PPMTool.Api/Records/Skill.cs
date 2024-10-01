31245e9ab425e59c66d751e16a4fb868c7aebe18
Chris Lam <fdq09eca@gmail.com>
2024-10-01 02:02:57 +0100

    init

---
+++ PPMTool.Api/Records/Skill.cs
@@ -0,0 +1,15 @@
+﻿using PPMTool.Data.Entities;
+
+namespace PPMTool.Api.Records;
+
+internal record Skill
+{
+    public string Name { get; init; }
+    public int SkillTagId { get; init; }
+
+    public Skill(SkillTag tag)
+    {
+        Name = tag.Name;
+        SkillTagId = tag.SkillTagId;
+    }
+}
