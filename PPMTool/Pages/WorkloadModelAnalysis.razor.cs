fde8c5c8f32a24bf67b6e8501d114daf7deca9b6
AdrianHarwood <adrian.harwood@manchester.ac.uk>
2024-10-10 11:29:45 +0100

    Basic file read on placeholder page

--- PPMTool/Pages/WorkloadModelAnalysis.razor.cs
+++ PPMTool/Pages/WorkloadModelAnalysis.razor.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using ApexCharts;
@@ -7,6 +9,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components;
 using PPMTool.Data;
 using PPMTool.Services;
+using Radzen;
 
 namespace PPMTool.Pages
 {
@@ -42,6 +45,85 @@ namespace PPMTool.Pages
 
             // Start the spinner
             Loading = true;
+        }
+
+        private async void OnClientChange(UploadChangeEventArgs args)
+        {
+            Debug.WriteLine("** Client-side upload changed");
+
+            foreach (var file in args.Files)
+            {
+                if (file.ContentType != "text/plain")
+                {
+                    throw new Exception("File is not of type text/plain as expected!");
+                }
+
+                Debug.WriteLine($"** File: {file.Name} / {file.Size} bytes");
+
+                try
+                {
+                    // Open stream
+                    using (var stream = file.OpenReadStream())
+                    {
+                        // Bail or read from stream
+                        if (stream == null) throw new IOException("Could not open file!");
+
+                        // Read into memory
+                        var bytes = new byte[stream.Length];
+                        await stream.ReadAsync(bytes);
+
+                        // Convert text
+                        var fileText = System.Text.Encoding.Default.GetString(bytes);
+                        var lines = fileText.Split("\n");
+
+                        // Read one line at a time
+                        var dates = new List<DateTime>();
+                        foreach (var line in lines)
+                        {
+                            // Split line
+                            var values = line.Split("\t");
+
+                            // Continue if no data on the line
+                            if (values.Length == 0) continue;
+
+                            // If the line starts with "Resource"
+                            if (values[0] == "Resource")
+                            {
+                                // Check it has the required neighbouring columns
+                                if (values[1] != "Activity" || values[2] != "Task" || !DateTime.TryParse(values[3], out var temp))
+                                {
+                                    throw new Exception("File needs to have columns named Resource, Activity, Task before the weekly data!");
+                                }
+
+                                // Parse the dates
+                                for (var week = 4; week < values.Length; week++)
+                                {
+                                    dates.Add(DateTime.Parse(values[week]));
+                                }
+
+                                // Move to next line
+                                continue;
+                            }
+
+                            // Generate an object from the line
+
+
+
+                            Debug.WriteLine($"** Read: {line}");
+                        }
+                    }
+
+                    // TODO: Now build the chart data arrays
+
+
+                }
+                catch (Exception ex)
+                {
+                    // TODO: Present an error notification to the user
+
+                    Debug.WriteLine($"** Client-side file read error: {ex.Message}");
+                }
+            }
         }
 
         private void GenerateCharts()
