ae154b5ab2b3c0b179a85ee844da5b9513ff8d1b
AdrianHarwood <adrian.harwood@manchester.ac.uk>
2024-10-17 16:33:42 +0100

    Created placeholder page

---
+++ PPMTool/Pages/CompetencyFramework.razor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+using PPMTool.Data.Entities;
+using PPMTool.Services;
+
+namespace PPMTool.Pages
+{
+    public partial class CompetencyFramework : BasePage
+    {
+        [Inject]
+        private PersonService PersonService { get; set; }
+
+        private IEnumerable<Person> people;
+
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            // Get starting lists from the DB
+            people = PersonService.GetAll(context);
+
+        }
+    }
+}
