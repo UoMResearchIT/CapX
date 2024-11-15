6d87e827bf5e716abeed342b150c164f79a2c47c
AdrianHarwood <adrian.harwood@manchester.ac.uk>
2024-11-15 16:00:05 +0000

    Separated out absences from manage people

---
+++ PPMTool/Pages/ViewAbsences.razor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using PPMTool.Data.Entities;
+using PPMTool.Services;
+
+namespace PPMTool.Pages
+{
+    [Authorize(Roles = "Manager,Superuser")]
+    public partial class ViewAbsences : BasePage
+    {
+        [Inject]
+        private PersonService PersonService { get; set; }
+
+        private IEnumerable<Absence> currentAbsences;
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            // Order by name by default
+            var loadedPeople = PersonService.GetAll(Context).OrderBy(x => x.Name).ToList();
+
+            // Current absences
+            currentAbsences = loadedPeople
+                .Where(x => x.IsCurrentlyAbsent())
+                .Select(x => x.Absences.FirstOrDefault(x => x.IsCurrentAbsence()));
+        }
+
+        private void EditAbsence(Person person)
+        {
+            Navigation.NavigateTo($"/addabsence/{person.PersonId}");
+        }
+    }
+}
