36bfb2da1019a717d7d1146dfb3eaa55207cbf13
PhilBradbury <phil.bradbury@manchester.ac.uk>
2024-11-23 13:31:17 +0000

    Register timesheet service

--- PPMTool/Startup.cs
+++ PPMTool/Startup.cs
@@ -61,6 +61,7 @@ namespace PPMTool
             services.AddScoped<NoteService>();
             services.AddScoped<FinancialReferenceService>();
             services.AddScoped<CompetencyService>();
+            services.AddScoped<TimesheetService>();
             services.AddTransient<ILogger>(s => s.GetRequiredService<ILogger<Startup>>());
 
             services.Configure<ForwardedHeadersOptions>(options =>
