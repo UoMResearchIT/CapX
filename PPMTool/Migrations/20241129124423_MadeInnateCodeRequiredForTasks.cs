ab060dcffe1104b81aba7ade2bdbca4f97773861
AdrianHarwood <adrian.harwood@manchester.ac.uk>
2024-11-29 12:45:17 +0000

    Added required flag for innate code on innate code tasks

---
+++ PPMTool/Migrations/20241129124423_MadeInnateCodeRequiredForTasks.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PPMTool.Migrations
+{
+    public partial class MadeInnateCodeRequiredForTasks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_InnateCodeTasks_InnateCodes_InnateCodeId",
+                table: "InnateCodeTasks");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "InnateCodeId",
+                table: "InnateCodeTasks",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_InnateCodeTasks_InnateCodes_InnateCodeId",
+                table: "InnateCodeTasks",
+                column: "InnateCodeId",
+                principalTable: "InnateCodes",
+                principalColumn: "InnateCodeId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_InnateCodeTasks_InnateCodes_InnateCodeId",
+                table: "InnateCodeTasks");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "InnateCodeId",
+                table: "InnateCodeTasks",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_InnateCodeTasks_InnateCodes_InnateCodeId",
+                table: "InnateCodeTasks",
+                column: "InnateCodeId",
+                principalTable: "InnateCodes",
+                principalColumn: "InnateCodeId");
+        }
+    }
+}
