333297363bdc2f97644fde8e1c7eebc9c2abdfb7
PhilBradbury <phil.bradbury@manchester.ac.uk>
2024-12-02 09:28:37 +0000

    Fix formatting

--- PPMTool/Pages/AddTimesheet.razor.cs
+++ PPMTool/Pages/AddTimesheet.razor.cs
@@ -92,7 +92,7 @@ namespace PPMTool.Pages
                 // Immediately save the timesheet to the DB
                 int newId = TimesheetService.Add(Context, timesheet);
 
-                if(newId == -1) { throw new Exception("Error creating new timesheet!"); }
+                if (newId == -1) { throw new Exception("Error creating new timesheet!"); }
 
                 // Redirect to the newly created Timesheet so refrshing the page
                 // with the -1 parameter doesn't create another new timesheet.
