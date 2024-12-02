22c7b554ac0ddf6396423186b1158e87277ce6ef
AdrianHarwood <adrian.harwood@manchester.ac.uk>
2024-12-02 11:13:31 +0000

    Added session store and load for switches

--- PPMTool/Pages/Timesheets.razor.cs
+++ PPMTool/Pages/Timesheets.razor.cs
@@ -1,9 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blazored.SessionStorage;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Mvc.Formatters;
 using PPMTool.Data.Entities;
 using PPMTool.Enums;
 using PPMTool.Services;
@@ -20,6 +20,9 @@ namespace PPMTool.Pages
         [Inject]
         private RolesService RolesService { get; set; }
 
+        [Inject]
+        private ISessionStorageService SessionStorage { get; set; }
+
         private Role userRole;
         private bool hideStaffResults = true;
         private bool showAllMyTimesheets;
@@ -31,6 +34,7 @@ namespace PPMTool.Pages
                 if (value != showAllMyTimesheets)
                 {
                     showAllMyTimesheets = value;
+                    SessionStorage.SetItemAsync<bool?>("timesheets-showall-mine", showAllMyTimesheets);
                     LoadData();
                 }
             }
@@ -45,6 +49,7 @@ namespace PPMTool.Pages
                 if (value != showAllMyStaffTimesheets)
                 {
                     showAllMyStaffTimesheets = value;
+                    SessionStorage.SetItemAsync<bool?>("timesheets-showall-reports", showAllMyStaffTimesheets);
                     LoadData();
                 }
             }
@@ -57,6 +62,12 @@ namespace PPMTool.Pages
         {
             await base.OnInitializedAsync();
             Loading = true;
+
+            // Load state from session storage
+            var temp = await SessionStorage.GetItemAsync<bool?>("timesheets-showall-mine");
+            if (temp != null) ShowAllMyTimesheets = temp ?? false;
+            temp = await SessionStorage.GetItemAsync<bool?>("timesheets-showall-reports");
+            if (temp != null) ShowAllMyStaffTimesheets = temp ?? false;
 
             // Look up the username
             var uname = AuthenticationState.User.Identity.Name.Trim().ToLower();
