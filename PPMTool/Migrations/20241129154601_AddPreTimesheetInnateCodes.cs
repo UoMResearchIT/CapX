7a913a4a5670e60e63f583be19d156936e108eb3
AdrianHarwood <adrian.harwood@manchester.ac.uk>
2024-12-12 15:46:30 +0000

    Removed context queries from migration

--- PPMTool/Migrations/20241129154601_AddPreTimesheetInnateCodes.cs
+++ PPMTool/Migrations/20241129154601_AddPreTimesheetInnateCodes.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Migrations;
-using Microsoft.Extensions.Configuration;
-using PPMTool.Data.Context;
-using PPMTool.Data.Entities;
 
 #nullable disable
 
@@ -18,61 +13,32 @@ namespace PPMTool.Migrations
             // Path to delimited file
             var filePath = $"./Migrations/Data/PreTimesheetInnateCodes.txt";
 
-            // Load the configuration from appsettings.json
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json")
-                .Build();
+            // Read all lines from the file
+            var lines = File.ReadAllLines(filePath);
 
-            // Get the connection string
-            var connectionString = configuration.GetConnectionString("PPMToolContextConnection");
-
-            // Create options for the custom DbContext
-            var optionsBuilder = new DbContextOptionsBuilder<PPMToolContext>();
-            optionsBuilder.UseSqlite(connectionString);
-
-            // Now have the context to check stuff
-            using (var context = new PPMToolContext(optionsBuilder.Options))
+            foreach (var line in lines)
             {
-                // Read all lines from the file
-                var lines = File.ReadAllLines(filePath);
+                // Split the line by the delimiter
+                var values = line.Split('|');
 
-                foreach (var line in lines)
+                if (values.Length != 2)
                 {
-                    // Split the line by the delimiter
-                    var values = line.Split('|');
-
-                    if (values.Length != 2)
-                    {
-                        throw new Exception("Incorrect number of values in line");
-                    }
-
-                    // Add the Innate code objects
-                    var obj = new InnateCode
-                    {
-                        ActivityCode = values[0].Trim(),
-                        ActivityName = values[1].Trim()
-                    };
-
-                    // Check no duplicate on code before trying to add it
-                    if (context.InnateCodes.Any(i => i.ActivityCode == obj.ActivityCode && i.ActivityName != obj.ActivityName))
-                    {
-                        throw new Exception($"Duplicate code found: {obj.ActivityCode} but with different name!");
-                    }
-                    // If both match then just leave it
-                    else if (context.InnateCodes.Any(i => i.ActivityCode == obj.ActivityCode && i.ActivityName == obj.ActivityName))
-                    {
-                        Console.WriteLine($"Code already exists with same name so no need to add: {obj.ActivityCode} - {obj.ActivityName}");
-                        continue;
-                    }
-                    else
-                    {
-                        context.InnateCodes.Add(obj);
-                    }
+                    throw new Exception("Incorrect number of values in line");
                 }
 
-                // Save changes
-                context.SaveChanges();
+                var activityCode = values[0].Trim().Replace("'", "''"); ;
+                var activityName = values[1].Trim().Replace("'", "''"); ;
+
+                var sqlCommand = $@"
+                    INSERT INTO InnateCodes (ActivityCode, ActivityName)
+                    SELECT '{activityCode}', '{activityName}'
+                    WHERE NOT EXISTS (
+                        SELECT 1 FROM InnateCodes WHERE ActivityCode = '{activityCode}' AND ActivityName = '{activityName}'
+                    );
+                ";
+
+                // Exceute sql command to add the record
+                migrationBuilder.Sql(sqlCommand);
             }
         }
 
