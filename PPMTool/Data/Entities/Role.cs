52bad824a5eefc574cada4eb5572732cbadb90d5
PhilBradbury <phil.bradbury@manchester.ac.uk>
2024-12-12 09:12:33 +0000

    New field added to the entity

--- PPMTool/Data/Entities/Role.cs
+++ PPMTool/Data/Entities/Role.cs
@@ -38,6 +38,8 @@ namespace PPMTool.Data.Entities
         [DataType(DataType.EmailAddress)]
         public string EmailAddress { get; set; }
 
+        public string TimesheetTemplateData { get; set; }
+
         public string GetSensibleObjectName()
         {
             return $"{GetName()} ({GetStandardisedUserName()})";
