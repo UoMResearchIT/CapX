a9e97e6b5bb35786bb8f692f78dce7b9fc5b33ea
AdrianHarwood <adrian.harwood@manchester.ac.uk>
2024-12-13 17:00:21 +0000

    Converted to top level statements and linked to use the existing skills tag model

---
+++ PPMTool.Api/Endpoints/Skills.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PPMTool.Data.Context;
+using PPMTool.Data.Entities;
+using PPMTool.Services;
+
+namespace PPMTool.API.Endpoints;
+
+/// <summary>
+/// Skill endpoint function mapping
+/// </summary>
+public static class Skills
+{
+    /// <summary>
+    /// Get all distinct skills 
+    /// </summary>
+    /// <returns>
+    /// A list of distinct skills
+    /// </returns> 
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<SkillTag>))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public static async Task<IResult> GetAll(PPMToolContext context, TagService tags)
+    {
+        var skillTags = await tags.GetAllAsync(context);
+        Debug.WriteLine($"** API: {skillTags.Count()} skill tags found.");
+        return Results.Json(skillTags);
+    }
+}
--- PPMTool.Api/Program.cs
+++ PPMTool.Api/Program.cs
@@ -3,64 +3,53 @@ using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 using PPMTool.Data.Context;
 using PPMTool.Services;
-namespace PPMTool.Api;
 
-class Program
-{
-    private static void Main(string[] args)
+var builder = WebApplication.CreateBuilder(args);
+
+// Access the configuration to get the connection string
+var configuration = builder.Configuration;
+
+builder.Services.AddDbContext<PPMToolContext>(options =>
+        options.UseSqlite(configuration.GetConnectionString("PPMToolContextConnection"))
+    );
+builder.Services.AddScoped<TagService>();
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(
+    opt =>
     {
-        var builder = WebApplication.CreateBuilder(args);
-
-
-
-        //builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-
-        builder.Services.AddDbContextFactory<PPMToolContext>(opt =>
-        {
-            opt.UseInMemoryDatabase("PPMTool"); // just for testing
-        });
-
-        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-        builder.Services.AddScoped<TagService>();
-
-        // Add services to the container.
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen(
-            opt =>
-            {
-                opt.SwaggerDoc(
-                    name: "v1",
-                    info: new() { Title = "PPMTool", Version = "v1" }
-                );
-
-                string? docFilePath = Directory.GetFiles(
-                    path: Directory.GetCurrentDirectory(),
-                    searchPattern: $"{Assembly.GetExecutingAssembly().GetName().Name}.xml",
-                    searchOption: SearchOption.AllDirectories)
-                .FirstOrDefault();
-
-                if (docFilePath != null)
-                {
-                    opt.IncludeXmlComments(docFilePath);
-                }
-                else
-                {
-                    Debug.Assert(false, "XML documentation file not found");
-                }
-            }
+        opt.SwaggerDoc(
+            name: "v1",
+            info: new() { Title = "PPMTool", Version = "v1" }
         );
 
-        var app = builder.Build();
+        string? docFilePath = Directory.GetFiles(
+            path: Directory.GetCurrentDirectory(),
+            searchPattern: $"{Assembly.GetExecutingAssembly().GetName().Name}.xml",
+            searchOption: SearchOption.AllDirectories)
+        .FirstOrDefault();
 
-        app.UseSwagger();
-        app.UseSwaggerUI();
-        app.UseHttpsRedirection();
-
-        // map endpoints to methods
-        app.MapGet("/skill/getAll", Endpoints.Skill.GetAll);
-
-
-        app.Run();
+        if (docFilePath != null)
+        {
+            opt.IncludeXmlComments(docFilePath);
+        }
+        else
+        {
+            Debug.Assert(false, "XML documentation file not found");
+        }
     }
-}
+);
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseHttpsRedirection();
+
+// Map endpoints to methods
+app.MapGet("/skills/getAll", PPMTool.API.Endpoints.Skills.GetAll);
+
+
+app.Run();
