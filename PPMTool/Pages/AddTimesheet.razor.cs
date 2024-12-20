babc279240758ff13db533ec7406bffe27019636
PhilBradbury <phil.bradbury@manchester.ac.uk>
2024-12-20 17:15:05 +0000

    Adjusted code to fetch the prev/next timesheet based on the timesheet's owner rather than the person viewing the page

--- PPMTool/Pages/AddTimesheet.razor.cs
+++ PPMTool/Pages/AddTimesheet.razor.cs
@@ -131,12 +131,13 @@ namespace PPMTool.Pages
 
                     if (timesheet != null)
                     {
-                        // Get details for the prev/next timesheets
+                        // Get details for the prev/next timesheets based on the owner of the timesheet being viewed
+                        // (to accommodate a manager looking at a staff timesheet).
                         previousTimesheet = null;
                         nextTimesheet = null;
                         DateTime previousDate = timesheet.StartDate.AddDays(-7);
                         DateTime nextDate = timesheet.StartDate.AddDays(7);
-                        List<Timesheet> prevandnext = TimesheetService.GetAllTimesheetsForPersonInDateRange(Context, ActiveUser, previousDate, nextDate).ToList();
+                        List<Timesheet> prevandnext = TimesheetService.GetAllTimesheetsForPersonInDateRange(Context, timesheet.Owner, previousDate, nextDate).ToList();
                         foreach (Timesheet t in prevandnext)
                         {
                             if (t.StartDate == previousDate) { previousTimesheet = t; }
