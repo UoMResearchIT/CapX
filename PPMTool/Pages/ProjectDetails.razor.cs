f75fcbcf2ff30d193578881a0c4da2e9f67a3a04
AdrianHarwood <adrian.harwood@manchester.ac.uk>
2025-01-18 15:58:30 +0000

    Updated the nav menu

--- PPMTool/Pages/ProjectDetails.razor.cs
+++ PPMTool/Pages/ProjectDetails.razor.cs
@@ -216,7 +216,7 @@ namespace PPMTool.Pages
                         {
                             Name = leadershipName,
                             StartDate = dateRange.StartDate,
-                            EndDate = dateRange.EndDate,
+                            EndDate = dateRange.EndDate.AddDays(-1),
                             OwningProject = project,
                             AssignedResources = new List<Resource>
                             {
