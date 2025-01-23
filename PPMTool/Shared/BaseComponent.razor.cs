a02feb71b1dc984c16e5565cfbcb6fb98de6f511
AdrianHarwood <adrian.harwood@manchester.ac.uk>
2025-01-23 15:25:54 +0000

    Abstracted out the authentication

---
+++ PPMTool/Shared/BaseComponent.razor.cs
@@ -0,0 +1,68 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.EntityFrameworkCore;
+using PPMTool.Data.Context;
+using PPMTool.Data.Entities;
+using PPMTool.Enums;
+using PPMTool.Services;
+
+namespace PPMTool.Shared
+{
+    public partial class BaseComponent : ComponentBase
+    {
+        [CascadingParameter]
+        protected Task<AuthenticationState> AuthenticationStateTask { get; set; }
+
+        protected AuthenticationState AuthenticationState { get; private set; }
+
+        protected PPMToolContext Context { get; set; }
+
+        protected string ActiveUserName { get; private set; } = "None";
+
+        public Person ActiveUser { get; private set; }
+
+        protected RoleType ActiveUserRoleType { get; private set; }
+
+        [Inject]
+        protected RolesService RolesService { get; set; }
+
+        [Inject]
+        protected IDbContextFactory<PPMToolContext> ContextFactory { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            if (AuthenticationState is not null)
+            {
+                var authState = AuthenticationState.GetAwaiter().GetResult();
+                var user = authState?.User;
+
+                if (user?.Identity is not null && user.Identity.IsAuthenticated)
+                {
+                    // Create the context on every page
+                    Context = ContextFactory.CreateDbContext();
+
+                    // Get authentication state
+                    AuthenticationState = AuthenticationStateTask.Result;
+
+                    // Stash the user name
+                    ActiveUserName = AuthenticationState?.User.Identity.Name.Trim().ToLower();
+
+                    // Get the active user
+                    var role = RolesService.GetByUsername(Context, ActiveUserName);
+                    ActiveUser = role?.Person;
+
+                    // Get active user role
+                    ActiveUserRoleType = role?.RoleType ?? Enums.RoleType.None;
+
+                    Debug.WriteLine($"Updating Timesheet notifications for {role.Person.Name}");
+                    TimesheetService.GetNotificationCount(context, role.Person);
+                }
+            }
+
+        }
+    }
+}
--- PPMTool/Shared/LoginView.razor.cs
+++ PPMTool/Shared/LoginView.razor.cs
@@ -2,33 +2,18 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Routing;
-using Microsoft.EntityFrameworkCore;
-using PPMTool.Data.Context;
 using PPMTool.Data.Entities;
 using PPMTool.Services;
 
 namespace PPMTool.Shared
 {
-    public partial class LoginView : ComponentBase, IDisposable
+    public partial class LoginView : BaseComponent, IDisposable
     {
         [Inject]
         private NavigationManager Navigation { get; set; }
-
-        [Inject]
-        private RolesService RoleService { get; set; }
-
-        [Inject]
-        private IDbContextFactory<PPMToolContext> ContextFactory { get; set; }
-
-        [Inject]
-        private TimesheetService TimesheetService { get; set; }
-
-        [CascadingParameter]
-        private Task<AuthenticationState> AuthenticationState { get; set; }
 
         private string displayName;
         private IEnumerable<Role> roles;
@@ -47,26 +32,14 @@ namespace PPMTool.Shared
 #if LOCAL
             showDropDown = true;
 #endif
-
             if (AuthenticationState is not null)
             {
                 var authState = AuthenticationState.GetAwaiter().GetResult();
                 var user = authState?.User;
 
-                if (user?.Identity is not null && user.Identity.IsAuthenticated)
+                if (user?.Identity is null || !user.Identity.IsAuthenticated)
                 {
-                    // Lookup the person
-                    PPMToolContext context = ContextFactory.CreateDbContext();
-                    var roles = RoleService.GetAll(context);
-                    var role = roles.FirstOrDefault(x => x.GetStandardisedUserName() == user.Identity.Name.Trim().ToLower());
-                    displayName = role?.GetName() ?? user.Identity.Name;
-
-                    Debug.WriteLine($"Updating Timesheet notifications for {role.Person.Name}");
-                    TimesheetService.GetNotificationCount(context, role.Person);
-                }
-                else
-                {
-                    roles = RoleService.GetAll(ContextFactory.CreateDbContext()).OrderByDescending(x => x.RoleType).ThenBy(x => x.Name);
+                    roles = RolesService.GetAll(ContextFactory.CreateDbContext()).OrderByDescending(x => x.RoleType).ThenBy(x => x.Name);
                     filteredRoles = roles.Select(x => RoleToString(x));
                     selectedRole = filteredRoles.FirstOrDefault();
                     OnChange();
