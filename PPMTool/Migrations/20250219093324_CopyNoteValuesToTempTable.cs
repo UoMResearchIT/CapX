1e334946c544e5f938aac900746adcb4679d5436
AdrianHarwood <adrian.harwood@manchester.ac.uk>
2025-02-19 09:35:56 +0000

    Added temp copy of notes key values

---
+++ PPMTool/Migrations/20250219093324_CopyNoteValuesToTempTable.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PPMTool.Migrations
+{
+    public partial class CopyNoteValuesToTempTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Copy the NoteID, AuthorId, and EditorId to a temporary table for each note
+            migrationBuilder.Sql(@"
+                CREATE TABLE TempNotes (
+                    NoteId INTEGER,
+                    AuthorId INTEGER,
+                    EditorId INTEGER
+                );
+
+                INSERT INTO TempNotes (NoteId, AuthorId, EditorId)
+                SELECT NoteId, AuthorPersonId, EditorPersonId
+                FROM Notes;
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DROP TABLE TempNotes
+            ");
+        }
+    }
+}
