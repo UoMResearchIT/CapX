57ed37cf17dd5969cdfae544ea3ab153f50a986f
AdrianHarwood <adrian.harwood@manchester.ac.uk>
2025-03-10 15:11:05 +0000

    Fixed up the roles

--- PPMTool/Enums/RoleType.cs
+++ PPMTool/Enums/RoleType.cs
@@ -2,10 +2,8 @@
 {
     public enum RoleType
     {
-        // Specify the int manually as the roles have changed for the ITS implementation.
-        // Saves having to remap anything in the database which references the role id
-        Contractor = 2,
-        Manager = 4,
-        Superuser = 5
+        Contractor,
+        Manager,
+        Superuser
     }
 }
