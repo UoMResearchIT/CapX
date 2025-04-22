720d7884c9477c6138f412220fe877c856aad57c
AdrianHarwood <adrian.harwood@manchester.ac.uk>
2025-04-22 17:58:07 +0100

    Added empty migration to deal with the inevitable migration of the cost model info

---
+++ PPMTool/Migrations/20250422165430_AdjustProjectLevelCostModels.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PPMTool.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdjustProjectLevelCostModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // TODO: Need to add a migration here to map the existing enum values to the new values now the values have simplified.
+            // TODO: Might need to do this in combination with adding resource-level settings of junior/standard rates?
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
