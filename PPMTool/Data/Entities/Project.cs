10514d8a1d54aefbd2b9d133cb25c0681071af75
AdrianHarwood <adrian.harwood@manchester.ac.uk>
2025-06-05 09:29:54 +0100

    Fixed conditions for budget error

--- PPMTool/Data/Entities/Project.cs
+++ PPMTool/Data/Entities/Project.cs
@@ -159,7 +159,7 @@ namespace PPMTool.Data.Entities
                 new StatusMessage("A current or future task in this project is under-resourced!", StatusMessage.MessageType.Warning, () => HasUnmetDemandInWindow()),
                 new StatusMessage("This project has started but has no link to a Scrum project!", StatusMessage.MessageType.Warning, () => HasStartedButHasNoScrumProjectLink()),
                 new StatusMessage("This project has no funding source for its leadership charges!", StatusMessage.MessageType.Error, () => HasNoLeadershipFundingSourcesButRan()),
-                new StatusMessage("This project has no agreed budget!", StatusMessage.MessageType.Error, () => Budget == 0),
+                new StatusMessage("This project has no agreed budget!", StatusMessage.MessageType.Error, () => HasNoBudget()),
                 new StatusMessage("A task in this project is running but the project is not active!", StatusMessage.MessageType.Error, () => RunningTaskButInactive()),
                 new StatusMessage("This project is active but has no currently running tasks!", StatusMessage.MessageType.Error, () => ActiveButNoRunningTask()),
                 new StatusMessage("This project has no project manager set!", StatusMessage.MessageType.Error, () => NotFinishedOrCancelledButNoPM()),
@@ -174,6 +174,15 @@ namespace PPMTool.Data.Entities
                 new StatusMessage("This project has a task with a resource without a funding source and is currently running or has run in the past!", StatusMessage.MessageType.Error, () => HasResourcesWithNoFundingSourceOnRunningTask()),
                 new StatusMessage("Everything looks OK!", StatusMessage.MessageType.Success, () => !HasActiveStatusMessages())
             };
+        }
+
+        /// <summary>
+        /// Determines whether the project has no budget but is not a new request when it legitimately might not have budget
+        /// </summary>
+        /// <returns></returns>
+        private bool HasNoBudget()
+        {
+            return Budget == 0 && ProjectStatus != ProjectStatus.NewRequest;
         }
 
         /// <summary>
