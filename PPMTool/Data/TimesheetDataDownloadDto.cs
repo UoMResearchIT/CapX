bb90994c2da80d0637df28c77988ba68c9f366d2
PhilBradbury <phil.bradbury@manchester.ac.uk>
2025-06-13 16:17:07 +0100

    The basic data we want to push into the spreadsheet

---
+++ PPMTool/Data/TimesheetDataDownloadDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static PPMTool.Enums.Extensions;
+
+namespace PPMTool.Data
+{
+    public class TimesheetDataDownloadDto
+    {
+        /// <summary>
+        /// Data transfer object which holds timesheet daily task data for the download feature.
+        /// </summary>
+        [ExcelHeader("Contractor Name")]
+        public string PersonName { get; set; }
+
+        [ExcelHeader("Date")]
+        public DateTime Date { get; set; }
+
+        [ExcelHeader("Hours Worked")]
+        public double HoursWorked { get; set; }
+    }
+}
