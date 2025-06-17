d984794af4e61b9210d925d8f9fc0e4acd5c54b1
PhilBradbury <phil.bradbury@manchester.ac.uk>
2025-06-17 13:34:42 +0100

    Added the button visible to the timesheet owner (and only when the timesheet is approved) to be able to download the image. Also added some ids to the HTML to allow hiding elements when generating the image

--- PPMTool/Pages/AddTimesheet.razor.cs
+++ PPMTool/Pages/AddTimesheet.razor.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using PPMTool.Data;
 using PPMTool.Data.Entities;
 using PPMTool.Enums;
@@ -32,6 +33,10 @@ namespace PPMTool.Pages
 
         [Inject]
         public EmailService EmailService { get; set; }
+
+        [Inject] 
+        public IJSRuntime JS { get; set; }
+
 
         private Timesheet timesheet;
         private IList<InnateCode> innateCodeDropdownSource = new List<InnateCode>();
@@ -300,6 +305,16 @@ namespace PPMTool.Pages
                 }
             }
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private async Task DownloadTimesheetImage()
+        {
+            await JS.InvokeVoidAsync("captureTimesheetAsImage");
+        }
+
 
         /// <summary>
         /// Method fired when the timesheet note is changed
