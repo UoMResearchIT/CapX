6dc42fb71d412d4068ba6299d0cfa3596df8b074
PhilBradbury <phil.bradbury@manchester.ac.uk>
2025-06-17 13:33:43 +0100

    Add reference to JS and the required method to create the image from the page

---
+++ PPMTool/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿function captureTimesheetAsImage() {
+    // Hide some elements
+    document.getElementById("timesheetButtons").style.display = "none";
+    document.getElementById("downloadScreenshotButton").style.display = "none";
+    document.getElementById("timesheetNoteAndComments").style.display = "none";
+    document.getElementById("sidebar").style.display = "none";
+
+    html2canvas(document.body).then(canvas => {
+        // Restore after capture
+        document.getElementById("timesheetButtons").style.display = "block";
+        document.getElementById("downloadScreenshotButton").style.display = "block";
+        document.getElementById("timesheetNoteAndComments").style.display = "block";
+        document.getElementById("sidebar").style.display = "block";
+
+        let link = document.createElement("a");
+        link.href = canvas.toDataURL("image/png");
+        link.download = "Timesheet.png";
+        link.click();
+    });
+}
