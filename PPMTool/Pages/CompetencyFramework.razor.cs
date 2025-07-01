21976c0bccc20e5b3c8e00e47e26b4f7b180b976
chandima131 <chandimasamarakoon.samankumari@manchester.ac.uk>
2025-07-01 11:22:15 +0100

    BugFix: Skip evidence requirement for unmet competencies

--- PPMTool/Pages/CompetencyFramework.razor.cs
+++ PPMTool/Pages/CompetencyFramework.razor.cs
@@ -655,12 +655,15 @@ namespace PPMTool.Pages
         /// <returns></returns>
         private bool ValidateAssessment(CompetencyAssessment assessment, out string message)
         {
-            if (string.IsNullOrWhiteSpace(assessment.Evidence) || string.IsNullOrWhiteSpace(HtmlHelper.ConvertToPlainText(assessment.Evidence)))
+            if (assessment.Status != AssessmentStatus.Unmet)
             {
-                message = "Evidence is required!";
-                return false;
+                if (string.IsNullOrWhiteSpace(assessment.Evidence) || string.IsNullOrWhiteSpace(HtmlHelper.ConvertToPlainText(assessment.Evidence)))
+                {
+                    message = "Evidence is required!";
+                    return false;
+                }
             }
-            else if (string.IsNullOrWhiteSpace(assessment.CompetencyDescription) || string.IsNullOrWhiteSpace(HtmlHelper.ConvertToPlainText(assessment.CompetencyDescription)))
+                else if (string.IsNullOrWhiteSpace(assessment.CompetencyDescription) || string.IsNullOrWhiteSpace(HtmlHelper.ConvertToPlainText(assessment.CompetencyDescription)))
             {
                 message = "Competency description is required!";
                 return false;
