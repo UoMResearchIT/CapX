ae21ed5c48ac57f95c498eb4ca9719fc52543d02
EAtbas <erdem.atbas@manchester.ac.uk>
2025-08-27 12:51:18 +0100

    feat: Convert the SkillsDTO to be more inline with the TimsheetsDTO

--- PPMTool.API/DTOs/PersonSkillsDTO.cs
+++ PPMTool.API/DTOs/PersonSkillsDTO.cs
@@ -1,31 +1,25 @@
-﻿using PPMTool.Data.Entities;
-
+﻿
 namespace PPMTool.API.DTOs
 {
     /// <summary>
-    /// DTO grouping person and their skills
+    /// DTOs for returning skills data.
     /// </summary>
-    public class PersonSkills
+    public class Skills
     {
         /// <summary>
-        /// Name of the person
+        /// A simplified representation of a SkillTag.
         /// </summary>
-        public string Name { get; }
+        public sealed record SkillTagDTO(
+            int SkillTagId,
+            string Name
+        );
 
         /// <summary>
-        /// Skills owned by the person
+        /// DTO grouping a person's name with their associated skills.
         /// </summary>
-        public IEnumerable<SkillTag> Skills { get; }
-
-        /// <summary>
-        /// Ctor
-        /// </summary>
-        /// <param name="name"></param>
-        /// <param name="skills"></param>
-        public PersonSkills(string name, IEnumerable<SkillTag> skills)
-        {
-            Name = name;
-            Skills = skills;
-        }
+        public sealed record PersonSkillsDTO(
+            string Name,
+            IEnumerable<SkillTagDTO> Skills
+        );
     }
 }
