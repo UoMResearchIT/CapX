d54d8ef635815e5defff31c46ff8ae3f3fd8b2ff
AdrianHarwood <adrian.harwood@manchester.ac.uk>
2025-08-29 10:46:40 +0100

    Moved records out of wrapper class as unnecessary

--- PPMTool.API/DTOs/PersonSkillsDTO.cs
+++ PPMTool.API/DTOs/PersonSkillsDTO.cs
@@ -2,24 +2,18 @@
 namespace PPMTool.API.DTOs
 {
     /// <summary>
-    /// DTOs for returning skills data.
+    /// DTO simplifying the representation of a skill tag.
     /// </summary>
-    public class Skills
-    {
-        /// <summary>
-        /// A simplified representation of a SkillTag.
-        /// </summary>
-        public sealed record SkillTagDTO(
-            int SkillTagId,
-            string Name
-        );
+    public sealed record SkillTagDTO(
+        int SkillTagId,
+        string Name
+    );
 
-        /// <summary>
-        /// DTO grouping a person's name with their associated skills.
-        /// </summary>
-        public sealed record PersonSkillsDTO(
-            string Name,
-            IEnumerable<SkillTagDTO> Skills
-        );
-    }
+    /// <summary>
+    /// DTO grouping a person's name with their owned skills.
+    /// </summary>
+    public sealed record PersonSkillsDTO(
+        string Name,
+        IEnumerable<SkillTagDTO> Skills
+    );
 }
