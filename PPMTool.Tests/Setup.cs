2d4b330092cffad3ba803cafd9f84b6aacf1d342
AdrianHarwood <adrian.harwood@manchester.ac.uk>
2025-09-27 08:53:25 +0100

    Added a test project with a simple test for the web app

---
+++ PPMTool.Tests/Setup.cs
@@ -0,0 +1,10 @@
+﻿namespace PPMTool.Tests
+{
+    [SetUpFixture]
+    public class Setup
+    {
+        // One time setup for the entire test run
+        // Web app needs to be running and available on port 5000 for these tests to work.
+        // It is not started automatically by the test framework.
+    }
+}
