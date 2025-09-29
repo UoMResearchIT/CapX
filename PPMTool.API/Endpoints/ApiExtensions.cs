aea1b4a77834b9565d286158aa8ffb53ad8a10a5
EAtbas <erdem.atbas@manchester.ac.uk>
2025-09-29 10:59:38 +0100

    feat: Create ApiExtensions for WLM functions

---
+++ PPMTool.API/Endpoints/ApiExtensions.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel;
+
+namespace PPMTool.API.Endpoints
+{
+    /// <summary>
+    /// Provides general extensions methods for common repeatedable actions in minimal API endpoints.
+    /// </summary>
+    public static class ApiExtensions
+    {
+        /// <summary>
+        /// Returns the date of the Monday of the week for the given date.
+        /// </summary>
+        public static DateTime StartOfWeek(this DateTime dt)
+        {
+            int diff = (7 + (dt.DayOfWeek - DayOfWeek.Monday)) % 7;
+            return dt.AddDays(-1 * diff).Date;
+        }
+
+        /// <summary>
+        /// Gets the string from an Enum's [Description] attribute.
+        /// </summary>
+        public static string GetDescription<T>(this T enumValue) where T : Enum
+        {
+            var field = enumValue.GetType().GetField(enumValue.ToString());
+            if (field == null) return enumValue.ToString();
+
+            var attribute = Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) as DescriptionAttribute;
+
+            return attribute == null ? enumValue.ToString() : attribute.Description;
+        }
+    }
+}
