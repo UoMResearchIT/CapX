194f0c1a84d11de3a02203d148c9a1593b015516
AdrianHarwood <adrian.harwood@manchester.ac.uk>
2025-10-10 10:28:17 +0100

    Added some placeholder components for a new estimation page using the cost model rates

---
+++ PPMTool/Pages/EstimateCost.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using PPMTool.Enums;
+
+namespace PPMTool.Pages
+{
+    [Authorize(Roles = "Manager,Superuser")]
+    public partial class EstimateCost : BasePage
+    {
+        private CostModel costModel;
+    }
+}
