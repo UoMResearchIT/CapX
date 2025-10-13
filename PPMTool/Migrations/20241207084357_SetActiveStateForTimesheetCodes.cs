270d21a8a7599d30d5bba7eae87a9a80eb37439d
AdrianHarwood <adrian.harwood@manchester.ac.uk>
2025-10-13 09:30:46 +0100

    Removed use of connection string and context objects from migration

--- PPMTool/Migrations/20241207084357_SetActiveStateForTimesheetCodes.cs
+++ PPMTool/Migrations/20241207084357_SetActiveStateForTimesheetCodes.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Linq.Dynamic.Core;
-using Microsoft.EntityFrameworkCore;
+﻿using System.Linq.Dynamic.Core;
 using Microsoft.EntityFrameworkCore.Migrations;
-using Microsoft.Extensions.Configuration;
-using PPMTool.Data.Context;
-using PPMTool.Enums;
 
 #nullable disable
 
@@ -15,74 +7,45 @@ namespace PPMTool.Migrations
 {
     public partial class SetActiveStateForTimesheetCodes : Migration
     {
-        List<string> activeCodes = new List<string>
-        {
-            "01",
-            "02",
-            "03",
-            "05",
-            "06",
-            "DMS",
-            "INC",
-            "P&A",
-            "S-RES006",
-            "S-RES007",
-            "S-RES011",
-            "S-RES012",
-            "S-RES013",
-            "S-RES014",
-            "S-RES015"
-        };
-
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            // Load the configuration from appsettings.json
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json")
-                .Build();
-
-            // Get the connection string
-            var connectionString = configuration.GetConnectionString("PPMToolContextConnection");
-
-            // Create options for the custom DbContext
-            var optionsBuilder = new DbContextOptionsBuilder<PPMToolContext>();
-            optionsBuilder.UseSqlite(connectionString);
-
-            // Now have the context to check stuff
-            using (var context = new PPMToolContext(optionsBuilder.Options))
+            // List of activity codes that should be active
+            var activeCodes = new[]
             {
-                // Go through all the timesheet codes
-                foreach (var code in context.InnateCodes)
-                {
-                    // Decide active
-                    bool shouldBeActive = IsOnExceptionList(code.ActivityCode) ||
-                        context.Projects.Any(x => x.InnateActivity.InnateCodeId == code.InnateCodeId &&
-                            x.ProjectStatus != ProjectStatus.Finished &&
-                            x.ProjectStatus != ProjectStatus.CancelledByCustomer &&
-                            x.ProjectStatus != ProjectStatus.CancelledBidFailed &&
-                            x.ProjectStatus != ProjectStatus.CancelledNoResource);
+                "01", "02", "03", "05", "06", "DMS", "INC", "P&A",
+                "S-RES006", "S-RES007", "S-RES011", "S-RES012",
+                "S-RES013", "S-RES014", "S-RES015"
+            };
 
-                    code.IsActive = shouldBeActive;
-                    if (shouldBeActive)
-                    {
-                        Console.WriteLine($"** Setting {code.GetCodeAsString()} to ACTIVE");
-                    }
-                }
+            // Format for SQL IN clause
+            var formattedCodes = string.Join(", ", activeCodes.Select(code => $"'{code.Replace("'", "''")}'"));
 
-                // Save changes
-                context.SaveChanges();
-            }
-        }
+            // SQL to set IsActive = 1 for codes in the exception list
+            migrationBuilder.Sql($@"
+                UPDATE InnateCodes
+                SET IsActive = 1
+                WHERE ActivityCode IN ({formattedCodes});
+            ");
 
-        private bool IsOnExceptionList(string activityCode)
-        {
-            return activeCodes.Contains(activityCode);
+            // SQL to set IsActive = 1 for codes linked to active projects
+            // Finished and cancelled states are 7-10 as enum values at the time
+            migrationBuilder.Sql(@"
+                UPDATE InnateCodes
+                SET IsActive = 1
+                WHERE InnateCodeId IN (
+                    SELECT ia.InnateCodeId
+                    FROM Projects p
+                    JOIN InnateCodes ia ON p.InnateActivityInnateCodeId = ia.InnateCodeId
+                    WHERE p.ProjectStatus NOT IN (7, 8, 9, 10)
+                );
+            ");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            // Reset IsActive to false although not sure what the original state was so not reversible
+            migrationBuilder.Sql("UPDATE InnateCodes SET IsActive = 0;");
         }
     }
+
 }
