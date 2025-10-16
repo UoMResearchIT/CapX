338992099c1983806c31776177964b451a9490e6
EAtbas <erdem.atbas@manchester.ac.uk>
2025-10-16 17:10:05 +0100

    fix: Convert the connection string to be EV

--- PPMTool.API/Helpers/EnvironmentHelper.cs
+++ PPMTool.API/Helpers/EnvironmentHelper.cs
@@ -23,6 +23,13 @@
                 overridingValues.Add("ConnectionStrings:PPMToolContextConnection", connectionString);
             }
 
+            // Leave bookings connection string override
+            var leaveBookingsConnectionString = Environment.GetEnvironmentVariable("LEAVEBOOKINGS_CONNECTION_STRING");
+            if (!string.IsNullOrWhiteSpace(leaveBookingsConnectionString))
+            {
+                overridingValues.Add("LeaveBookings:ConnectionString", leaveBookingsConnectionString);
+            }
+
             // Add the overriding values to the configuration
             builder.Configuration.AddInMemoryCollection(overridingValues!);
         }
