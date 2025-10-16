661d12dad6d73163dfadfe92e66c4e1c38614194
AdrianHarwood <adrian.harwood@manchester.ac.uk>
2025-10-16 20:46:39 +0100

    Programmatic adding of the task components

--- PPMTool/Pages/EstimateCost.razor.cs
+++ PPMTool/Pages/EstimateCost.razor.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using PPMTool.Enums;
+using PPMTool.Pages.Components;
 
 namespace PPMTool.Pages
 {
@@ -7,11 +8,43 @@ namespace PPMTool.Pages
     public partial class EstimateCost : BasePage
     {
         private CostModel costModel;
+        private CostModel CostModel
+        {
+            get => costModel;
+            set
+            {
+                if (costModel != value)
+                {
+                    costModel = value;
+                    UpdateComponentCostModels();
+                }
+            }
+        }
+
+        private Dictionary<string, TaskConfigurationComponent> resources = new Dictionary<string, TaskConfigurationComponent>
+        {
+            { "Leadership", new TaskConfigurationComponent() },
+            { "RSE 1", new TaskConfigurationComponent() },
+            { "RSE 2", new TaskConfigurationComponent() }
+        };
+
+        private TaskConfigurationComponent summaryTask;
 
         protected override void OnInitialized()
         {
             base.OnInitialized();
             Loading = false;
+        }
+
+        /// <summary>
+        /// Updates the cost model for all resource components when the selected cost model changes.
+        /// </summary>
+        private void UpdateComponentCostModels()
+        {
+            foreach (var resource in resources.Values)
+            {
+                resource.CostModel = CostModel;
+            }
         }
     }
 }
