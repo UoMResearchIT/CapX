91ca7a1accc61d365580537f9efc2ac703166b0c
EAtbas <erdem.atbas@manchester.ac.uk>
2025-10-16 17:10:48 +0100

    feat: Update the LeaveBooking to be sealed record

--- PPMTool.API/DTOs/LeaveBookingsDTO.cs
+++ PPMTool.API/DTOs/LeaveBookingsDTO.cs
@@ -1,72 +1,23 @@
 namespace PPMTool.API.DTOs
 {
     /// <summary>
-    /// A Staff entry from the central system.
+    /// A staff leave summary sourced from the central leave booking system.
     /// </summary>
-    public class LeaveBookingsDTO
-    {
-        /// <summary>
-        /// Employeed Id : UoM staff number (or an actual string in the case of the Admin account)
-        /// </summary>
-        public string EmployeeId { get; set; }
-
-        /// <summary>
-        /// If of the user's Line Managaer
-        /// </summary>
-        public int SupervisorId { get; set; }
-
-        /// <summary>
-        /// UoM username
-        /// </summary>
-        public string Username { get; set; }
-
-        /// <summary>
-        /// User's forename
-        /// </summary>
-        public string FirstName { get; set; }
-
-        /// <summary>
-        /// User's surname
-        /// </summary>
-        public string LastName { get; set; }
-
-        /// <summary>
-        /// Base Leave allowance for a specified year
-        /// </summary>
-        public double CoreAllowance { get; set; } = 0.0;
-
-        /// <summary>
-        /// Allowance adjustment for the year of concern
-        /// </summary>
-        public double Adjustment { get; set; } = 0.0;
-
-        /// <summary>
-        /// The user's bookings for the year of concern
-        /// </summary>
-        public List<BookingDTO> Bookings { get; set; } = new();
-
-        /// <summary>
-        /// Class constructor : only sets the detail known on first use
-        /// </summary>
-        /// <param name="employeeId"></param>
-        /// <param name="supervisorId"></param>
-        /// <param name="username"></param>
-        /// <param name="firstName"></param>
-        /// <param name="lastName"></param>
-        public LeaveBookingsDTO(string employeeId, int supervisorId, string username, string firstName, string lastName)
-        {
-            EmployeeId = employeeId;
-            SupervisorId = supervisorId;
-            Username = username;
-            FirstName = firstName;
-            LastName = lastName;
-        }
-    }
+    public sealed record LeaveBookingsDTO(
+        string EmployeeId,
+        int SupervisorId,
+        string Username,
+        string FirstName,
+        string LastName,
+        double CoreAllowance,
+        double Adjustment,
+        IReadOnlyList<BookingDTO> Bookings
+    );
 
     /// <summary>
-    /// A Leave Booking entry
+    /// A single leave booking entry.
     /// </summary>
-    public record BookingDTO(
+    public sealed record BookingDTO(
         DateTime Date,
         string AmPm
     );
