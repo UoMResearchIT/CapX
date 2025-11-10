88300ce56a7efead96626f260cc036d8e34ebdff
AdrianHarwood <adrian.harwood@manchester.ac.uk>
2025-11-10 14:44:33 +0000

    Just in case name is null

--- PPMTool/Shared/BaseComponent.razor.cs
+++ PPMTool/Shared/BaseComponent.razor.cs
@@ -42,7 +42,7 @@ namespace PPMTool.Shared
                     if (Context == null) Context = ContextFactory.CreateDbContext();
 
                     // Get the user object
-                    User user = UserService.GetByUsernameOrEmail(Context, claimsPrincipal.Identity.Name.Trim().ToLower());
+                    User user = UserService.GetByUsernameOrEmail(Context, claimsPrincipal.Identity.Name?.Trim().ToLower());
 
                     // Get the active user
                     ActiveUser = user;
