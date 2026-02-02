e0626092506b821e76914baa37c3420a948976d9
PhilBradbury <phil.bradbury@manchester.ac.uk>
2026-02-02 07:53:50 +0000

    Added classes for Faculty and School. Simplest implementation for UoM which can be reconfigured for other places once in the db.

--- PPMTool/Data/Entities/Faculty.cs
+++ PPMTool/Data/Entities/Faculty.cs
@@ -1,18 +1,54 @@
-﻿namespace PPMTool.Data.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace PPMTool.Data.Entities
 {
     /// <summary>
     /// Represents an institution or faculty which owns a number of <see cref="School"/>
     /// </summary>
-    public class Faculty : BaseOrgUnit
+    public class Faculty : ILoggableClass
     {
-        /// <summary>
-        /// Primary key
-        /// </summary>
         public int FacultyId { get; set; }
+
+        /// <summary>
+        /// Name of the organisational unit.
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Abbreviated name or code of the organisational unit.
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// Description to help identify it to admins
+        /// </summary>
+        [Required]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// A value to be used to control the ordering in dropdowns.
+        /// Defaults to zero and in that case the Id will be used instead.
+        /// </summary>
+        [Required]
+        public int Order { get; set; } = 0;
+
+        /// <summary>
+        /// For soft deletion/visibility toggling
+        /// </summary>
+        [Required]
+        public bool IsActive { get; set; } = true;
+
+        public string GetSensibleObjectName()
+        {
+            return (Code != null && Code.Length > 0 ? $"{Name} ({Code})" : $"{Name}");
+        }
 
         /// <summary>
         /// Navigation property for the schools or departments within this faculty
         /// </summary>
+        [Required]
         public virtual ICollection<School> Schools { get; set; } = new List<School>();
     }
 }
--- PPMTool/Data/Entities/School.cs
+++ PPMTool/Data/Entities/School.cs
@@ -5,15 +5,50 @@ namespace PPMTool.Data.Entities
     /// <summary>
     /// Represents a school or department within a <see cref="Data.Entities.Faculty" />
     /// </summary>
-    public class School : BaseOrgUnit
+    public class School : ILoggableClass
     {
         public int SchoolId { get; set; }
+
+        /// <summary>
+        /// Name of the organisational unit.
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Abbreviated name or code of the organisational unit.
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// Description to help identify it to admins
+        /// </summary>
+        [Required]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// A value to be used to control the ordering in dropdowns.
+        /// Defaults to zero and in that case the Id will be used instead.
+        /// </summary>
+        [Required]
+        public int Order { get; set; } = 0;
+
+        /// <summary>
+        /// For soft deletion/visibility toggling
+        /// </summary>
+        [Required]
+
+        public bool IsActive { get; set; } = true;
+
+        public string GetSensibleObjectName()
+        {
+            return (Code != null && Code.Length > 0 ? $"{Name} ({Code})" : $"{Name}");
+        }
 
         /// <summary>
         /// The faculty to which this school or department belongs
         /// </summary>
         [Required]
         public Faculty Faculty { get; set; }
-
     }
 }
