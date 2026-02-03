61998dc22722a1786db5e5d2a82b7c4c90b35998
PhilBradbury <phil.bradbury@manchester.ac.uk>
2026-02-03 13:50:42 +0000

    Added CRUD classes for Schools and Faculties

---
+++ PPMTool/Services/FacultyService.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using PPMTool.Data.Context;
+using PPMTool.Data.Entities;
+
+namespace PPMTool.Services
+{
+    public class FacultyService : BaseEntityService<Faculty>
+    {
+        /// <summary>
+        /// Returns all faculties in the DB
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override IEnumerable<Faculty> GetAll(PPMToolContext context)
+        {
+            return context.Faculties;
+        }
+
+        /// <summary>
+        /// Returns faculties in the DB filtered by their IsActive status
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public IEnumerable<Faculty> GetAll(PPMToolContext context, bool isActive)
+        {
+            return GetAll(context).Where(x => x.IsActive == isActive);
+        }
+
+        /// <summary>
+        /// Updates the faculty in the DB
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="faculty"></param>
+        public override int Update(PPMToolContext context, Faculty faculty, bool commitChanges = true)
+        {
+            if (DuplicateDetected(context, faculty))
+            {
+                return -1;
+            }
+            context.Faculties.Update(faculty);
+            if (commitChanges) CommitChanges(context);
+            return faculty.FacultyId;
+        }
+
+        /// <summary>
+        /// Gets the tracking entry for the faculty
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="faculty"></param>
+        /// <returns></returns>
+        internal EntityEntry<Faculty> GetEntry(PPMToolContext context, Faculty faculty)
+        {
+            return context.Entry(faculty);
+        }
+
+        /// <summary>
+        /// Deletes a faculty from the DB
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="faculty"></param>
+        public override void Delete(PPMToolContext context, Faculty faculty, bool commitChanges = true)
+        {
+            context.Remove(faculty);
+            if (commitChanges) CommitChanges(context);
+        }
+
+        /// <summary>
+        /// Adds a new faculty to the DB
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="faculty"></param>
+        public override int Add(PPMToolContext context, Faculty faculty, bool commitChanges = true)
+        {
+            if (DuplicateDetected(context, faculty))
+            {
+                return -1;
+            }
+            context.Add(faculty);
+            if (commitChanges) CommitChanges(context);
+            return faculty.FacultyId;
+        }
+
+        /// <summary>
+        /// Detect a duplicate faculty
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public override bool DuplicateDetected(PPMToolContext context, Faculty entity)
+        {
+            return GetAll(context).Any(x => (Clean(x.Name) == Clean(entity.Name) || Clean(x.Code) == Clean(entity.Code)) && x.FacultyId != entity.FacultyId);
+        }
+    }
+}
---
+++ PPMTool/Services/SchoolService.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using PPMTool.Data.Context;
+using PPMTool.Data.Entities;
+
+namespace PPMTool.Services
+{
+    public class SchoolService : BaseEntityService<School>
+    {
+        /// <summary>
+        /// Returns schools in the DB (active ones only, by default)
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override IEnumerable<School> GetAll(PPMToolContext context)
+        {
+            return context.Schools;
+        }
+
+        /// <summary>
+        /// Returns schools in the DB filtered by their IsActive status
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public IEnumerable<School> GetAll(PPMToolContext context, bool isActive)
+        {
+            return GetAll(context).Where(x => x.IsActive == isActive);
+        }
+
+        /// <summary>
+        /// Updates the school in the DB
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="school"></param>
+        public override int Update(PPMToolContext context, School school, bool commitChanges = true)
+        {
+            if (DuplicateDetected(context, school))
+            {
+                return -1;
+            }
+            context.Schools.Update(school);
+            if (commitChanges) CommitChanges(context);
+            return school.SchoolId;
+        }
+
+        /// <summary>
+        /// Gets the tracking entry for the school
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="school"></param>
+        /// <returns></returns>
+        internal EntityEntry<School> GetEntry(PPMToolContext context, School school)
+        {
+            return context.Entry(school);
+        }
+
+        /// <summary>
+        /// Deletes a school from the DB
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="school"></param>
+        public override void Delete(PPMToolContext context, School school, bool commitChanges = true)
+        {
+            context.Remove(school);
+            if (commitChanges) CommitChanges(context);
+        }
+
+        /// <summary>
+        /// Adds a new school to the DB
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="school"></param>
+        public override int Add(PPMToolContext context, School school, bool commitChanges = true)
+        {
+            if (DuplicateDetected(context, school))
+            {
+                return -1;
+            }
+            context.Add(school);
+            if (commitChanges) CommitChanges(context);
+            return school.SchoolId;
+        }
+
+        /// <summary>
+        /// Detect a duplicate school
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public override bool DuplicateDetected(PPMToolContext context, School entity)
+        {
+            return GetAll(context).Any(x => (Clean(x.Name) == Clean(entity.Name) || Clean(x.Code) == Clean(entity.Code)) && x.SchoolId != entity.SchoolId);
+        }
+    }
+}
