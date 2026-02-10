f2f11a5a5955cac55431c88293e17e408e94d6a2
PhilBradbury <phil.bradbury@manchester.ac.uk>
2026-02-10 14:41:51 +0000

    Migrations added to create Faculty and School tables and update Projects table to reference them

---
+++ PPMTool/Migrations/20260206160202_AddFacultyAndSchoolTables.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PPMTool.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFacultyAndSchoolTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //
+            // 1. Create Faculties table
+            //
+            migrationBuilder.CreateTable(
+                name: "Faculties",
+                columns: table => new
+                {
+                    FacultyId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Code = table.Column<string>(type: "TEXT", nullable: true),
+                    Order = table.Column<int>(type: "INTEGER", nullable: false),
+                    IsActive = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Faculties", x => x.FacultyId);
+                });
+
+            //
+            // 2. Create Schools table
+            //
+            migrationBuilder.CreateTable(
+                name: "Schools",
+                columns: table => new
+                {
+                    SchoolId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FacultyId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Code = table.Column<string>(type: "TEXT", nullable: true),
+                    Order = table.Column<int>(type: "INTEGER", nullable: false),
+                    IsActive = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schools", x => x.SchoolId);
+                    table.ForeignKey(
+                        name: "FK_Schools_Faculties_FacultyId",
+                        column: x => x.FacultyId,
+                        principalTable: "Faculties",
+                        principalColumn: "FacultyId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            //
+            // 3. Seed Faculties (including None)
+            //
+            migrationBuilder.InsertData(
+                table: "Faculties",
+                columns: new[] { "FacultyId", "Name", "Description", "Code", "Order", "IsActive" },
+                values: new object[,]
+                {
+            { 1, "None", "", "None", 0, true },
+
+            { 2, "Research IT / Internal", "", "Internal", 1, true },
+            { 3, "Professional Services and Cultural Institutions", "", "PSCI", 2, true },
+            { 4, "Biology, Medicine and Health", "", "FBMH", 3, true },
+            { 5, "Humanities", "", "FHUMS", 4, true },
+            { 6, "Science and Engineering", "", "FSE", 5, true },
+            { 7, "Research Lifecycle Programme", "", "RLP", 6, true },
+            { 8, "Commercial / External", "", "External", 7, true },
+            { 9, "Cross-Faculty Research Institutes", "", "ResInst", 8, true }
+                });
+
+            //
+            // 4. Seed Schools (including None)
+            //
+            migrationBuilder.InsertData(
+                table: "Schools",
+                columns: new[] { "SchoolId", "FacultyId", "Name", "Description", "Code", "Order", "IsActive" },
+                values: new object[,]
+                {
+            { 1, 1, "None", "", "None", 0, true },
+
+            // FSE
+            { 2, 6, "School of Engineering", "", "SE", 1, true },
+            { 3, 6, "School of Natural Sciences", "", "SNS", 2, true },
+
+            // FBMH
+            { 4, 4, "School of Biological Sciences", "", "SBS", 1, true },
+            { 5, 4, "School of Medical Sciences", "", "SMS", 2, true },
+            { 6, 4, "School of Health Sciences", "", "SHS", 3, true },
+
+            // FHUMS
+            { 7, 5, "Alliance Manchester Business School", "", "AMBS", 1, true },
+            { 8, 5, "School of Arts, Languages and Cultures", "", "SALC", 2, true },
+            { 9, 5, "School of Environment, Education and Development", "", "SEED", 3, true },
+            { 10, 5, "School of Social Sciences", "", "SSS", 4, true }
+                });
+
+            //
+            // 5. Add one to the enum values
+            //
+            migrationBuilder.Sql(
+                @"
+                    UPDATE Projects
+                    SET Faculty = Faculty + 1;
+                    UPDATE Projects
+                    SET School = School + 1;
+                "
+            );
+            
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Schools");
+
+            migrationBuilder.DropTable(
+                name: "Faculties");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Projects_FacultyId",
+                table: "Projects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Projects_SchoolId",
+                table: "Projects");
+
+            migrationBuilder.Sql(
+                @"
+                    UPDATE Projects
+                    SET Faculty = Faculty - 1;
+                    UPDATE Projects
+                    SET School = School - 1;
+                "
+            );
+        }
+    }
+}
---
+++ PPMTool/Migrations/20260210143044_UpdateProjectTableToUseNewTables.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PPMTool.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateProjectTableToUseNewTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //
+            // 1. Rename old string columns to new FK columns
+            //
+            migrationBuilder.RenameColumn(
+                name: "School",
+                table: "Projects",
+                newName: "SchoolId");
+
+            migrationBuilder.RenameColumn(
+                name: "Faculty",
+                table: "Projects",
+                newName: "FacultyId");
+
+            //
+            // 2. Add foreign keys
+            //
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_Faculties_FacultyId",
+                table: "Projects",
+                column: "FacultyId",
+                principalTable: "Faculties",
+                principalColumn: "FacultyId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_Schools_SchoolId",
+                table: "Projects",
+                column: "SchoolId",
+                principalTable: "Schools",
+                principalColumn: "SchoolId",
+                onDelete: ReferentialAction.Cascade);
+
+            //
+            // 3. Create indexes
+            //
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_FacultyId",
+                table: "Projects",
+                column: "FacultyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_SchoolId",
+                table: "Projects",
+                column: "SchoolId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schools_FacultyId",
+                table: "Schools",
+                column: "FacultyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            //
+            // 1. Drop foreign keys
+            //
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_Faculties_FacultyId",
+                table: "Projects");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_Schools_SchoolId",
+                table: "Projects");
+
+            //
+            // 2. Drop indexes
+            //
+            migrationBuilder.DropIndex(
+                name: "IX_Projects_FacultyId",
+                table: "Projects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Projects_SchoolId",
+                table: "Projects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Schools_FacultyId",
+                table: "Schools");
+
+            //
+            // 3. Drop Schools and Faculties tables
+            //
+            migrationBuilder.DropTable(
+                name: "Schools");
+
+            migrationBuilder.DropTable(
+                name: "Faculties");
+
+            //
+            // 4. Rename FK columns back to original string columns
+            //
+            migrationBuilder.RenameColumn(
+                name: "SchoolId",
+                table: "Projects",
+                newName: "School");
+
+            migrationBuilder.RenameColumn(
+                name: "FacultyId",
+                table: "Projects",
+                newName: "Faculty");
+        }
+    }
+}
