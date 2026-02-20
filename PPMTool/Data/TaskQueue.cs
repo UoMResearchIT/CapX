27cf369f8a0e36afde11b77622c385a96da10361
EAtbas <erdem.atbas@manchester.ac.uk>
2026-02-20 11:45:04 +0000

    chore: Follow the workflow style

--- PPMTool/Data/TaskQueue.cs
+++ PPMTool/Data/TaskQueue.cs
@@ -1,6 +1,5 @@
 // SPDX-FileCopyrightText: 2026 University of Manchester
 //
-// SPDX-License-Identifier: Apache-2.0
 // SPDX-License-Identifier: apache-2.0
 
 using System.Collections.Concurrent;
--- deployment/dbbackup.sh
+++ deployment/dbbackup.sh
@@ -2,7 +2,6 @@
 
 # SPDX-FileCopyrightText: 2026 University of Manchester
 #
-# SPDX-License-Identifier: Apache-2.0
 # SPDX-License-Identifier: apache-2.0
 
 set -eu
