c5bee6230b7f22737aae34f17516c181912612ff
PhilBradbury <phil.bradbury@manchester.ac.uk>
2026-03-23 10:11:49 +0000

    Added a simple method to get a list of all enabled features to pass into StatusMessage lists so they can calculate whether they need to be displayed

--- PPMTool/Services/FeatureService.cs
+++ PPMTool/Services/FeatureService.cs
@@ -61,5 +61,16 @@ namespace PPMTool.Services
             }
             return false;
         }
+
+        /// <summary>
+        /// Get a list of all enabled features
+        /// </summary>
+        /// <returns></returns>
+        public List<FeatureType> GetEnabledFeatures()
+        {
+            return FeatureState.Where(kvp => kvp.Value)
+                               .Select(kvp => kvp.Key)
+                               .ToList();
+        }
     }
 }
