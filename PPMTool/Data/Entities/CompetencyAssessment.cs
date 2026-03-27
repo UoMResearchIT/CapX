409b742909a5418656b0333fb61121173462e4f5
AdrianHarwood <adrian.harwood@manchester.ac.uk>
2026-03-27 14:40:42 +0000

    Removed unused clone methods

--- PPMTool/Data/Entities/CompetencyAssessment.cs
+++ PPMTool/Data/Entities/CompetencyAssessment.cs
@@ -57,38 +57,6 @@ namespace PPMTool.Data.Entities
         public int PersonId { get; set; }
 
         /// <summary>
-        /// Copy all editable property values from another assessment onto this instance.
-        /// Used when applying changes from a detached editing clone back onto a tracked entity,
-        /// and when creating a detached clone for editing.
-        /// </summary>
-        /// <param name="source">The assessment to copy values from.</param>
-        public void CopyValuesFrom(CompetencyAssessment source)
-        {
-            Evidence = source.Evidence;
-            DateCreated = source.DateCreated;
-            Status = source.Status;
-            CompetencyRevision = source.CompetencyRevision;
-            CompetencyDescription = source.CompetencyDescription;
-            CompetencyObjective = source.CompetencyObjective;
-            CompetencyId = source.CompetencyId;
-            PersonId = source.PersonId;
-        }
-
-        /// <summary>
-        /// Create a detached copy of this assessment for editing without affecting EF Core tracking.
-        /// </summary>
-        /// <returns>A new <see cref="CompetencyAssessment"/> with the same property values.</returns>
-        public CompetencyAssessment Clone()
-        {
-            var clone = new CompetencyAssessment
-            {
-                CompetencyAssessmentId = CompetencyAssessmentId
-            };
-            clone.CopyValuesFrom(this);
-            return clone;
-        }
-
-        /// <summary>
         /// A sensible object name for logging purposes
         /// </summary>
         /// <returns></returns>
