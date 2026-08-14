81ce6b952134880175e9622af0eafaf33f4bafe1
AdrianHarwood <adrian.harwood@manchester.ac.uk>
2026-08-14 11:38:24 +0100

    Switched to shadow properties to ensure filtering and sorting works

--- PPMTool/Models/FinanceSummaryItem.cs
+++ PPMTool/Models/FinanceSummaryItem.cs
@@ -76,11 +76,25 @@ namespace PPMTool.Models
 
         public double ActualHours { get; set; }
 
+        public double? DayRateForGrid => CostModel == CostModel.DayRate ? DayRate : null;
+
         public double BudgetDirectCosts => Budget - BudgetIndirectCosts;
+
+        public double? BudgetDirectCostsForGrid => CostModel.HasIndirects() ? BudgetDirectCosts : Budget;
+
+        public double? BudgetIndirectCostsForGrid => CostModel.HasIndirects() ? BudgetIndirectCosts : null;
 
         public double PlannedTechnicalCosts => GetTechPlannedCosts();
 
+        public double? PlannedLeadershipCostsForGrid => CostModel.HasLeadership() ? PlannedLeadershipCosts : null;
+
+        public double? PlannedIndirectCostsForGrid => CostModel.HasIndirects() ? PlannedIndirectCosts : null;
+
         public double ActualTechnicalCosts => GetTechActualCosts();
+
+        public double? ActualLeadershipCostsForGrid => CostModel.HasLeadership() ? ActualLeadershipCosts : null;
+
+        public double? ActualIndirectCostsForGrid => CostModel.HasIndirects() ? ActualIndirectCosts : null;
 
         public double FundsRequestedTotal => GetAllRequested();
 
